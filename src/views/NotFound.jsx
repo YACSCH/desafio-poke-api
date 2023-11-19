2d397e2841a6c239930fe04624d9d252045f7c2a
Yamil Castillo <yamilcs@gmail.com>
2023-11-18 22:45:44 -0300

    creando pagina NotFound - desafio pokeApi

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
   return (
     <Container>
       <h1 className='text-center mt-5 pt-5'>Upss la pagina que buscas, no existe!!!!!</h1>
-      <div className='text-center my-2'>
+      <div className='text-center mt-5 pt-5 pt-2'>
         <Button
           variant='danger'
           onClick={() => {
