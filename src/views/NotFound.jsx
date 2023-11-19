ad7d161c0894eae739150148c920ff7a0d4d2c1b
Yamil Castillo <yamilcs@gmail.com>
2023-11-18 22:46:23 -0300

    creando pagina NotFound - desafio pokeApi

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -3,7 +3,9 @@ import Button from 'react-bootstrap/Button'
 import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+  
   const navigate = useNavigate()
+
   return (
     <Container>
       <h1 className='text-center mt-5 pt-5'>Upss la pagina que buscas, no existe!!!!!</h1>
